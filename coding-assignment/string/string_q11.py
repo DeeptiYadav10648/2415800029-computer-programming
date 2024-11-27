c5c92a19796013aaa324163ba85a035de8bb7538
DeeptiYadav10648 <deepti10648@gmail.com>
2024-11-27 21:44:29 +0530

    Create string_q11.py

---
+++ coding-assignment/string/string_q11.py
@@ -0,0 +1,11 @@
+#11 Find All Permutations of a String
+st = input()
+n=len(st)
+for i in range(2**n):
+    comb=""
+    for j in range(n):
+        if i & (2**j):
+            comb += st[j]
+        else:
+            comb +='0'
+    print(comb[::-1])
