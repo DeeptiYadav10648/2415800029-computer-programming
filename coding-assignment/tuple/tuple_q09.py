874c92aea65fa5a0ee6625d8f63a91eb6e03f4ea
DeeptiYadav10648 <deepti10648@gmail.com>
2024-11-28 08:26:25 +0530

    Update tuple_q09.py

--- coding-assignment/tuple/tuple_q09.py
+++ coding-assignment/tuple/tuple_q09.py
@@ -1,1 +1,11 @@
+#Find the union, intersection, and difference of two tuples:
+t1 = (1, 2, 3)
+t2 = (3, 4, 5)
 
+union = tuple(set(t1).union(t2))
+intersection = tuple(set(t1).intersection(t2))
+difference = tuple(set(t1).difference(t2))
+
+print("Union:", union)
+print("Intersection:", intersection)
+print("Difference:", difference)
